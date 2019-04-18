31b548f56db2497978a31d11fb8d2a35c1fdfb7e
monyarm <monyarm@gmail.com>
2019-04-18 11:46:09 +0300

    Updated Books.ts to latest typescript and ts-node

--- books.ts
+++ books.ts
@@ -1,56 +1,57 @@
-import * as fs from "fs";
+import fs from 'fs';
 class Book {
-  title: string;
-  translated_title?: string;
-  category: string[];
-  sources?: string[];
-  description?: string;
-  author?: string;
+  public title: string;
+  // tslint:disable-next-line: variable-name
+  public translated_title?: string;
+  public category: string[];
+  public sources?: string[];
+  public description?: string;
+  public author?: string;
 }
-var books: Array<Book> = [];
-var dir = __dirname + "/books";
-bookReader(dir);
-function bookReader(dir) {
-  dir = dir + "/";
-  fs.readdirSync(dir).forEach(function(file) {
+let books: Book[] = [];
+const directory = __dirname + '/books';
+bookReader(directory);
+function bookReader(dir = '') {
+  dir = dir + '/';
+  fs.readdirSync(dir).forEach(file => {
     if (fs.lstatSync(dir + file).isDirectory()) {
       bookReader(dir + file);
     } else {
-      books = books.concat(require(dir + file).books);
+      if (file.endsWith('.json')) {
+        books = books.concat(require(dir + file).books);
+      }
     }
   });
 }
 function printPretty(arr: Book[]) {
-  console.log("---------");
+  console.log('---------');
   arr.forEach(book => {
-    console.log("Title: " + book.title);
+    console.log('Title: ' + book.title);
     if (book.translated_title) {
-      console.log("Translated Title: " + book.translated_title);
+      console.log('Translated Title: ' + book.translated_title);
     }
     if (book.author) {
-      console.log("Author: " + book.author);
+      console.log('Author: ' + book.author);
     }
     if (book.category) {
       book.category.forEach(cat => {
-        console.log("Category: " + cat);
+        console.log('Category: ' + cat);
       });
     }
     if (book.description) {
-      console.log("Description: " + book.description);
+      console.log('Description: ' + book.description);
     }
-    console.log("---------");
+    console.log('---------');
   });
 }
-function checkDuplicateInObject(propertyName, inputArray) {
-  var seenDuplicate = false,
-    testObject = {};
-  var dupes: Book[] = [];
-  inputArray.map(function(item) {
-    var itemPropertyName = item[propertyName];
+function checkDuplicateInObject(propertyName: string, inputArray: any) {
+  const testObject: any = {};
+  const dupes: Book[] = [];
+  inputArray.map((item: { [x: string]: any; duplicate: boolean; title: Book; }) => {
+    const itemPropertyName = item[propertyName];
     if (itemPropertyName in testObject) {
       testObject[itemPropertyName].duplicate = true;
       item.duplicate = true;
-      seenDuplicate = true;
       dupes.push(item.title);
     } else {
       testObject[itemPropertyName] = item;
@@ -60,8 +61,8 @@ function checkDuplicateInObject(propertyName, inputArray) {
 
   return dupes;
 }
-function shuffle(a) {
-  var b = a;
+function shuffle(a: any[] | Book[]) {
+  const b = a;
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [b[i], b[j]] = [a[j], a[i]];
@@ -70,16 +71,15 @@ function shuffle(a) {
 }
 
 switch (process.argv[2]) {
-  case "log":
+  case 'log':
     console.log(books.length);
     break;
-  case "random":
-    printPretty(shuffle(books).slice(0, process.argv[3]));
+  case 'random':
+    printPretty(shuffle(books).slice(0, Number(process.argv[3])));
     break;
-  case "dupe":
-    console.log(checkDuplicateInObject("title", books));
+  case 'dupe':
+    console.log(checkDuplicateInObject('title', books));
     break;
   default:
     break;
 }
-
