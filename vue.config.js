5d986547024b2338771f22cb3a287fa7b170fa15
monyarm <monyarm@gmail.com>
2020-03-22 02:41:18 +0200

    fixes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV == 'production' ? __dirname + '/dist/' :
+      '/Book-Keeper/',
   configureWebpack: {
     resolve: {
       alias: {
